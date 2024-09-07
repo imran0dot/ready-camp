f13af4b7bc0d352f25f58c132bfaa9c26c650526
imran0dot <imran0dot@gmail.com>
2024-09-07 21:26:27 +0600

    matrial ui setup complite

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { Provider } from 'react-redux'
+import { store } from './redux/store.ts'
+import { ThemeProvider } from "@material-tailwind/react";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ThemeProvider>
   </StrictMode>,
 )
