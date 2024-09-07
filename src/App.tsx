a8563bd2d74a148301be65fd86d20c8b9cdc0f2d
imran0dot <imran0dot@gmail.com>
2024-09-07 21:42:24 +0600

    header footer added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import './App.css'
+import Footer from './components/shared/footer/Footer'
+import Header from './components/shared/header/Header'
 
 function App() {
   return (
     <>
-
+      <Header />
+      <Footer />
     </>
   )
 }
