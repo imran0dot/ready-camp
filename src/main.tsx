46f0aa7c31a82d850bf04efc8d978799d4921868
imran0dot <imran0dot@gmail.com>
2024-09-07 21:50:11 +0600

    react router dom setup complete

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import { store } from './redux/store.ts'
 import { ThemeProvider } from "@material-tailwind/react";
+import router from './router/index.tsx'
+import { RouterProvider } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider>
       <Provider store={store}>
-        <App />
+        <RouterProvider router={router} />
       </Provider>
     </ThemeProvider>
   </StrictMode>,
