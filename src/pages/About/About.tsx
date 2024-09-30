574834a852ab74e3ba6436d6de2288d6b88ec008
imran0dot <imran0dot@gmail.com>
2024-10-01 02:03:22 +0600

    about us added new section

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import ContactForm from '../../components/forms/ContactForm';
+import { Typography } from '@material-tailwind/react';
 
 const About: React.FC = () => {
     return (
@@ -13,6 +15,93 @@ const About: React.FC = () => {
                     <img className="w-full h-full" src="https://i.ibb.co/FhgPJt8/Rectangle-116.png" alt="A group of People" />
                 </div>
             </div>
+
+            <section className="bg-blue-gray-50 dark:bg-slate-800 mt-10 p-20 rounded-md" id="contact">
+                <div className="mx-auto">
+                    <div className="mb-4">
+                        <div className="mb-6 max-w-3xl text-center sm:text-center md:mx-auto md:mb-12">
+                            <p className="text-base font-semibold uppercase tracking-wide text-blue-600 dark:text-blue-200">
+                                Contact
+                            </p>
+                            <h2
+                                className="font-heading mb-4 font-bold tracking-tight text-gray-900 dark:text-white text-3xl sm:text-5xl">
+                                Get in Touch
+                            </h2>
+                            <p className="mx-auto mt-4 max-w-3xl text-xl text-gray-600 dark:text-slate-400">In hac habitasse platea
+                                dictumst
+                            </p>
+                        </div>
+                    </div>
+                    <div className="flex items-stretch justify-center">
+                        <div className="grid md:grid-cols-2">
+                            <div className="h-full pr-6">
+                                <p className="mt-3 mb-12 text-lg text-gray-600 dark:text-slate-400">
+                                    Class aptent taciti sociosqu ad
+                                    litora torquent per conubia nostra, per inceptos himenaeos. Duis nec ipsum orci. Ut scelerisque
+                                    sagittis ante, ac tincidunt sem venenatis ut.
+                                </p>
+                                <ul className="mb-6 md:mb-0">
+                                    <li className="flex">
+                                        <div className="flex h-10 w-10 items-center justify-center rounded bg-blue-900 text-gray-50">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                                fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                                stroke-linejoin="round" className="h-6 w-6">
+                                                <path d="M9 11a3 3 0 1 0 6 0a3 3 0 0 0 -6 0"></path>
+                                                <path
+                                                    d="M17.657 16.657l-4.243 4.243a2 2 0 0 1 -2.827 0l-4.244 -4.243a8 8 0 1 1 11.314 0z">
+                                                </path>
+                                            </svg>
+                                        </div>
+                                        <div className="ml-4 mb-4">
+                                            <h3 className="mb-2 text-lg font-medium leading-6 text-gray-900 dark:text-white">Our Address
+                                            </h3>
+                                            <p className="text-gray-600 dark:text-slate-400">1230 Maecenas Street Donec Road</p>
+                                            <p className="text-gray-600 dark:text-slate-400">New York, EEUU</p>
+                                        </div>
+                                    </li>
+                                    <li className="flex">
+                                        <div className="flex h-10 w-10 items-center justify-center rounded bg-blue-900 text-gray-50">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                                fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                                stroke-linejoin="round" className="h-6 w-6">
+                                                <path
+                                                    d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2">
+                                                </path>
+                                                <path d="M15 7a2 2 0 0 1 2 2"></path>
+                                                <path d="M15 3a6 6 0 0 1 6 6"></path>
+                                            </svg>
+                                        </div>
+                                        <div className="ml-4 mb-4">
+                                            <h3 className="mb-2 text-lg font-medium leading-6 text-gray-900 dark:text-white">Contact
+                                            </h3>
+                                            <p className="text-gray-600 dark:text-slate-400">Mobile: +1 (123) 456-7890</p>
+                                            <p className="text-gray-600 dark:text-slate-400">Mail: tailnext@gmail.com</p>
+                                        </div>
+                                    </li>
+                                    <li className="flex">
+                                        <div className="flex h-10 w-10 items-center justify-center rounded bg-blue-900 text-gray-50">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                                fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                                stroke-linejoin="round" className="h-6 w-6">
+                                                <path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0"></path>
+                                                <path d="M12 7v5l3 3"></path>
+                                            </svg>
+                                        </div>
+                                        <div className="ml-4 mb-4">
+                                            <h3 className="mb-2 text-lg font-medium leading-6 text-gray-900 dark:text-white">Working
+                                                hours</h3>
+                                            <p className="text-gray-600 dark:text-slate-400">Monday - Friday: 08:00 - 17:00</p>
+                                            <p className="text-gray-600 dark:text-slate-400">Saturday &amp; Sunday: 08:00 - 12:00</p>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+
+                            <ContactForm />
+                        </div>
+                    </div>
+                </div>
+            </section>
 
             <div className="flex lg:flex-row flex-col justify-between gap-8 pt-12">
                 <div className="w-full lg:w-5/12 flex flex-col justify-center">
@@ -43,6 +132,14 @@ const About: React.FC = () => {
                         </div>
                     </div>
                 </div>
+            </div>
+
+
+            <div className='mt-20'>
+                <Typography className='text-3xl font-bold mb-5 text-blue-gray-900'>
+                    Our Store location 
+                </Typography>
+                <iframe className='w-full rounded-2xl' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3651.367032992344!2d90.35574967432673!3d23.76994098802785!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c19704f97af7%3A0xab7638e90fa88e4c!2sUmmah%20Point!5e0!3m2!1sen!2sbd!4v1727725472599!5m2!1sen!2sbd" height="450" loading="lazy"></iframe>
             </div>
         </div>
 
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -12,6 +12,7 @@ import PeterStromImage from "../../assets/peter-strom.webp"
 import halfPriceImage from "../../assets/half-price.webp"
 import ProductCard from '../../components/widgets/ProductCard';
 import ContactForm from '../../components/forms/ContactForm';
+import { Typography } from '@material-tailwind/react';
 
 
 type TCategory = {
@@ -144,7 +145,34 @@ const Home: React.FC = () => {
             {/* Contact us section  */}
             <section>
                 <Wrapper>
-                    <ContactForm />
+                    <section className="px-8 py-8 lg:py-16">
+                        <div className="container mx-auto text-center">
+                            <Typography
+                                variant="h5"
+                                color="blue-gray"
+                                className="mb-4 !text-base lg:!text-2xl"
+                            >
+                                Customer Care
+                            </Typography>
+                            <Typography
+                                variant="h1"
+                                color="blue-gray"
+                                className="mb-4 !text-3xl lg:!text-5xl"
+                            >
+                                We&apos;re Here to Help
+                            </Typography>
+                            <Typography className="mb-10 font-normal !text-lg lg:mb-20 mx-auto max-w-3xl !text-gray-500">
+                                Whether it&apos;s a question about our services, a request for
+                                technical assistance, or suggestions for improvement, our team is
+                                eager to hear from you.
+                            </Typography>
+                            <div className="grid grid-cols-1 gap-x-12 gap-y-6 lg:grid-cols-2 items-start">
+                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3651.367032992344!2d90.35574967432673!3d23.76994098802785!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c19704f97af7%3A0xab7638e90fa88e4c!2sUmmah%20Point!5e0!3m2!1sen!2sbd!4v1727725472599!5m2!1sen!2sbd" width="600" height="450" loading="lazy"></iframe>
+
+                                <ContactForm />
+                            </div>
+                        </div>
+                    </section>
                 </Wrapper>
             </section>
 
